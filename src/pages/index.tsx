48e4f49f52a6ce594a4890a4f62ee7abd45343f3
Artur Ribeiro <profissional.arturrc@gmail.com>
2023-05-14 19:34:03 -0300

    :arrow_up: Install eslint configs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Heading } from '@ignite-ui/react'
 
 export default function Home() {
-  return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, exercitationem.
-    </div>
-  )
+  return <Heading as="h1">Hello World</Heading>
 }
